2a3b1a5e4565df97a664a42a5a71b3f5aa223334
Youngpyo Sim <kileuno123@gmail.com>
2022-12-08 11:43:04 +0900

    이름 입력 받기 추가

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
-const test = 10;
+const headerUserNameElement = document.querySelector('.user');
+const userNameElement = document.querySelector('.name');
+const localUserName = localStorage.getItem('userName');
 
-console.log(test);
+const setUserNameInnerHtml = (name) => {
+  headerUserNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameElement.innerHTML = `${name} <span>님</span>`;
+};
+
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
+}
+
+userNameElement.onclick = () => {
+  const userName = prompt('이름을 입력해 주세요');
+  // todo: userName에 대한 유효성 검사
+  localStorage.setItem('userName', userName);
+  setUserNameInnerHtml(userName);
+};
+
+
+console.log(userNameElement);
+console.log(userNameElement.textContent);
+console.log(userNameElement.innerHTML);
