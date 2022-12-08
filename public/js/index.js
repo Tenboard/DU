b764f9ad00407df506759fbdb2fe06feb3718fd9
Youngpyo Sim <kileuno123@gmail.com>
2022-12-08 11:53:52 +0900

    클릭 영역 범위 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,18 @@
 const headerUserNameElement = document.querySelector('.user');
 const userNameElement = document.querySelector('.name');
 const localUserName = localStorage.getItem('userName');
+const userNameBtn = document.querySelector('#user-name');
 
 const setUserNameInnerHtml = (name) => {
   headerUserNameElement.innerHTML = `${name} <span>님</span>`;
-  userNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameBtn.textContent = name;
 };
 
 if (localUserName) {
   setUserNameInnerHtml(localUserName);
 }
 
-userNameElement.onclick = () => {
+userNameBtn.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
   // todo: userName에 대한 유효성 검사
   localStorage.setItem('userName', userName);
